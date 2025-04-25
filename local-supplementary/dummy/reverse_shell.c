c219c8e54bdb9c38a7d12177e08a60c6d8c96016
VishvadiniRavihari <120405053+Zury7@users.noreply.github.com>
2025-04-25 17:06:07 +0530

    dummy commit

---
+++ local-supplementary/dummy/reverse_shell.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // Hardcoded values for a and b
+    int a = 5;
+    int b = 7;
+    
+    // Perform addition
+    int c = a + b;
+    
+    // Output the result
+    // printf("The result of %d + %d = %d\n", a, b, c);
+    
+    return 0;
+}
